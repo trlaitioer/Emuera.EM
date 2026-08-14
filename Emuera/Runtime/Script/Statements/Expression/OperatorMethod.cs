280926ab502da53ef49deca8c4eed4cfa9a1d217
trlaitioer <trlaitioer@gmail.com>
2026-08-15 00:58:35 +0800

    fix(script): correct string >=/<= comparison and unary minus double evaluation

--- Emuera/Runtime/Script/Statements/Expression/OperatorMethod.cs
+++ Emuera/Runtime/Script/Statements/Expression/OperatorMethod.cs
@@ -475,7 +475,7 @@ internal static class OperatorMethodManager
 		public override long GetIntValue(ExpressionMediator exm, List<AExpression> arguments)
 		{
 			int c = string.Compare(arguments[0].GetStrValue(exm), arguments[1].GetStrValue(exm), Config.Config.SCExpression);
-			if (c < 0)
+			if (c >= 0)
 				return 1L;
 			return 0L;
 		}
@@ -506,7 +506,7 @@ internal static class OperatorMethodManager
 		public override long GetIntValue(ExpressionMediator exm, List<AExpression> arguments)
 		{
 			int c = string.Compare(arguments[0].GetStrValue(exm), arguments[1].GetStrValue(exm), Config.Config.SCExpression);
-			if (c < 0)
+			if (c <= 0)
 				return 1L;
 			return 0L;
 		}
@@ -696,7 +696,7 @@ internal static class OperatorMethodManager
 			{
 				exm.Console.PrintSystemLine(string.Format(Lang.SystemLine.MinusWontWork.Text, long.MinValue));
 			}
-			return -arguments[0].GetIntValue(exm);
+			return -ret;
 		}
 	}
 
